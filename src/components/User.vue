903d2919b01b1682cf317b2ef91e95da3ff6e234
Gabriela Parada <gabrielaparada@Gabrielas-MacBook-Pro.local>
2020-07-07 21:58:41 -0400

    passing data through props

---
+++ src/components/User.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my name</button>
+        <p>Name is {{ name }}</p>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <!-- passing data from the parent to the child -->
+                <app-user-detail :myName="name" @nameWasReset="name = $event"></app-user-detail> 
+                <!-- namewasreset is the name of the event, name is the name of the component were we are storing the data on our data function, $event refers to the data that was passed through the event -->
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function(){
+            return{
+                name: 'Gaby'
+            }
+        },
+        methods: {
+            changeName(){
+                this.name = 'Cam'
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User name: {{ switchName()}}</p>
+        <button @click="resetName">Reset</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            myName: String
+        }, //this should be called the same as the word we are passing in our double moustche
+        methods: {
+            switchName(){
+                //we can use props and access it as a normal property using the this keyword
+                return this.myName.split("").reverse().join("")
+            },
+            resetName(){
+                this.myName = "Gaby"
+
+                //the first argument you pass to emit is a name of your choice and the second one is the data you want to change 
+                //passing data from the child to the parent
+                this.$emit('nameWasReset',this.myName)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
